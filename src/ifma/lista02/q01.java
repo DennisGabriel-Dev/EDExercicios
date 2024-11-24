3c761fff174c1168321ff0caac82c18a047200ff
DennisGabriel <dennisgabr745@gmail.com>
2024-11-24 00:01:01 -0300

    add q01 by list02

---
+++ src/ifma/lista02/q01.java
@@ -0,0 +1,23 @@
+package ifma.lista02;
+
+public class q01{
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if(matriz[i][j] == matriz[i][i]){
+                    System.out.print("0 ");
+                }
+                else{
+                    System.out.print(matriz[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
