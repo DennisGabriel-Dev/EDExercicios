dc2993fd51b9c51f20138e9b08826952c0706ad7
DennisGabriel <dennisgabr745@gmail.com>
2025-02-06 08:59:24 -0300

    exemplo de Counting sort

---
+++ src/couting_sort/Main.java
@@ -0,0 +1,28 @@
+package couting_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 1, 2, 7, 5, 2};
+        int max = Arrays.stream(arr).max().getAsInt();
+        int[] count = new int[max + 1];
+
+        for (int num : arr) {
+            count[num]++;
+        }
+
+        System.out.println("Array de contagem: " + Arrays.toString(count));
+
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[index] = i;
+                index++;
+                count[i]--;
+            }
+        }
+
+        System.out.println("Array ordenado: " + Arrays.toString(arr));
+    }
+}
